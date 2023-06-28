f74ad586547fe99e0cd62085e1ef02e0844e915c
root <root@Ians-Domain>
2023-06-28 18:02:39 -0400

    game menu progress

--- lib/games.ex
+++ lib/games.ex
@@ -1,18 +1,42 @@
 defmodule Games do
-  @moduledoc """
-  Documentation for `Games`.
-  """
 
-  @doc """
-  Hello world.
+  # Prompt the user to select which game they want to play
 
-  ## Examples
+  def prompt() do
+    IO.gets("""
 
-      iex> Games.hello()
-      :world
+    What game would you like to play?
+    1. Guessing Game
+    2. Rock Paper Scissors
+    3. Wordle
 
-  """
-  def hello do
-    :world
+    enter "stop" to exit
+    """)
+    |> to_string()
+    |> String.trim_trailing()
+  end
+
+  defp choose_game() do
+    choice = prompt()
+
+    case choice do
+      "1" ->
+        Games.GuessingGame.play()
+        choose_game()
+
+      "2" ->
+        Games.RockPaperScissors.play()
+        choose_game()
+
+      "3" ->
+        Games.Wordle.play()
+        choose_game()
+
+      "stop" ->
+        IO.puts("Goodbye!")
+
+      _ ->
+        choose_game()
+    end
   end
 end
